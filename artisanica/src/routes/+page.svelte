e80f2cf5709dd44bf1086f9d39dfaceb6db22284
Emad Dehnavi <dehnavi@gmx.com>
2025-05-11 12:43:41 +0200

    styling the login page

--- artisanica/src/routes/+page.svelte
+++ artisanica/src/routes/+page.svelte
@@ -1,11 +1,13 @@
-<div class="flex flex-row w-full h-screen bg-blue-950 items-center justify-center">
-    <div class=" flex flex-col items-center justify-center w-full h-full bg-blue-800 gap-4">
-    <form class="flex flex-col items-center justify-center w-full h-full gap-4">
-        <h1 class="text-3xl text-center text-white">Welcome to Artisanica</h1>
-        <input type="text" placeholder="Username" class="p-2 mb-4 border border-gray-300 rounded" />
-        <input type="text" placeholder="Password" class="p-2 mb-4 border border-gray-300 rounded" />
+<div class="flex flex-row w-full h-screen bg-[#F9FAFB] items-center justify-center">
+    <div class=" flex flex-col items-center justify-center w-full h-ful gap-4">
+    <form class="flex flex-col items-center justify-center w-fit h-fit p-8 gap-4 bg-[#fff] rounded-lg shadow-lg">
+        <h1 class="text-3xl text-[#1F2937] text-center">Welcome to Artisanica</h1>
+        <input type="text" placeholder="Username" class="p-2 mb-4 border border-gray-300 rounded bg-[#E5E7EB]" />
+        <input type="text" placeholder="Password" class="p-2 mb-4 border border-gray-300 rounded bg-[#E5E7EB]" />
 
-        <button class="px-4 py-2 text-white bg-blue-600 rounded">Sign in</button>
+        <a href="/register" class="text-[#3B82F6] hover:underline">Don't have an account? Register</a>
+
+        <button class="px-4 py-2 bg-[#1F2937] text-[#fff] rounded">Sign in</button>
     </form>
     </div>
 </div>
