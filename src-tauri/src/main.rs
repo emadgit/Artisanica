bd07eafc6c983496031ce30c4aea825cb35f0b37
Emad Dehnavi <dehnavi@gmx.com>
2025-05-11 10:46:13 +0200

    change of framework

--- src-tauri/src/main.rs
+++
@@ -1,8 +0,0 @@
-mod db;
-
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#[cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
-fn main() {
-    artisanica_lib::run()
-}
