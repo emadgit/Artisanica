29b3b81fd6cf5b670b2ad4514c7c28df4190fd90
Emad Dehnavi <dehnavi@gmx.com>
2025-05-11 12:26:59 +0200

    initial login screen

--- artisanica/src/routes/+page.svelte
+++ artisanica/src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="flex flex-row w-full h-screen bg-blue-950 items-center justify-center">
+    <div class=" flex flex-col items-center justify-center w-full h-full bg-blue-800 gap-4">
+    <form class="flex flex-col items-center justify-center w-full h-full gap-4">
+        <h1 class="text-3xl text-center text-white">Welcome to Artisanica</h1>
+        <input type="text" placeholder="Username" class="p-2 mb-4 border border-gray-300 rounded" />
+        <input type="text" placeholder="Password" class="p-2 mb-4 border border-gray-300 rounded" />
+
+        <button class="px-4 py-2 text-white bg-blue-600 rounded">Sign in</button>
+    </form>
+    </div>
+</div>
